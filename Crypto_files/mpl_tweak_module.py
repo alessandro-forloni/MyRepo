41c6af8173112a7cd91992ef9a31ee9a6d901c22
alessandro-forloni <alessandro.forloni@epfl.ch>
2018-02-12 09:10:47 +0100

    Added default settings in case of reset

--- Crypto_files/mpl_tweak_module.py
+++ Crypto_files/mpl_tweak_module.py
@@ -15,16 +15,27 @@ import numpy as np
 #DEFINE GLOBAL FEATURES
 
 rc_dict = \
-{'axes.edgecolor':'green', \
- 'axes.facecolor':'black', \
- 'ytick.color':'green',  \
- 'xtick.color':'green',  \
- 'grid.color':'grey',    \
- 'grid.linestyle':'--',  \
- 'grid.linewidth': 0.4,    \
- 'figure.facecolor':'black' \
+{'axes.edgecolor':'green',   \
+ 'axes.facecolor':'black',   \
+ 'ytick.color':'green',      \
+ 'xtick.color':'green',      \
+ 'grid.color':'grey',        \
+ 'grid.linestyle':'--',      \
+ 'grid.linewidth': 0.4,      \
+ 'figure.facecolor':'black'  \
  }
 
+
+default_dict = \
+{'axes.edgecolor':'black',   \
+ 'axes.facecolor':'white',   \
+ 'ytick.color':'k',          \
+ 'xtick.color':'k',          \
+ 'grid.color':'b0b0b0',      \
+ 'grid.linestyle':'-',       \
+ 'grid.linewidth': 0.8,      \
+ 'figure.facecolor':'white'  \
+ }
 #Keep dictionary of default features
 
 
@@ -42,7 +53,7 @@ def reset():
     
     for x in rc_dict:
         
-        mpl.rcParams[x] = rc_dict[x]
+        mpl.rcParams[x] = default_dict[x]
 
     
     
